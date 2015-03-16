07508d3756e6f09684bff4db1ddf14a02ebc6b72
dramamoose <dramamoose@users.noreply.github.com>
2015-03-16 15:10:21 -0600

    Add comments, finish program

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Create a list of functions which allows a Matrix and the inverse of that matrix
+# to be cached in the operating (non-local) environment
 
-## Write a short comment describing this function
+##makeCacheMatrix: Creates a number of functions for a matrix which can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL #Set M to null
+  set <- function(y) { #Set the value of our matrix and reset m in the parent environment
+    x <<- y 
+    m <<- NULL
+  }
+  get <- function() x #Returns the value of our matrix 
+  setsolve <- function(solve) m <<- solve #Sets the inverse of our matrix
+  getsolve <- function() m #Returns the inverse of our matrix
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve) #Create a list of all four of these functions
+}  
 
-}
 
 
-## Write a short comment describing this function
+##cacheSolve: Returns the inverse of the matrix 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve() 
+  if(!is.null(m)) { #If we already have cached data for M
+    message("getting cached data") #Let us know we aren't recalculating
+    return(m) #And provide us with the inverted matrix
+  }
+  data <- x$get()  #Otherwise, pull the matrix
+  m <- solve(data, ...) #Then reverse it
+  x$setsolve(m) #And cache it in x
+  m #Then display the inverted matrix
 }
